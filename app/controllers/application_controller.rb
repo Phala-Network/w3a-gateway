caa713b913ee5c579dcbe369406fdbe7bd87cd7e
jasl <jasl9187@hotmail.com>
2020-02-26 02:17:11 +0800

    bug fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
-  rescue_from SQLite3::BusyException do
-    ActiveRecord::Base.connection.execute("BEGIN TRANSACTION; END;")
-  end
 end
